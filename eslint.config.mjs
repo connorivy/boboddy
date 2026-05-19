a160035c2095b2eda54c0e23b997a12eaf2360fd
connorivy <connorivy15@utexas.edu>
2026-05-19 06:13:30 -0500

    fix release pipeline

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,6 @@ export default tseslint.config([
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
-    ignores: ["**/dist/**", "**/node_modules/**", "**/generated/**"],
+    ignores: ["**/dist/**", "**/node_modules/**", "**/generated/**", "docs/**"],
   },
 ]);
